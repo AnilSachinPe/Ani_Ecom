f6df95412b3ec4eea1da315de4f3d1a62c6de138
AnilSachinPe <44719601+AnilSachinPe@users.noreply.github.com>
2020-12-25 12:02:39 +0530

    Updates

---
+++ precache-manifest.35665dfa096df1ff8a75628fb05e36ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3746c4c555591d274ebe81329be3d564",
+    "url": "/ani-ecom/index.html"
+  },
+  {
+    "revision": "1f2d5705817210e29453",
+    "url": "/ani-ecom/static/css/main.3df08fc8.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/ani-ecom/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ani-ecom/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2d5705817210e29453",
+    "url": "/ani-ecom/static/js/main.37aeee2b.chunk.js"
+  },
+  {
+    "revision": "2f01ab2a889613a8bf0f",
+    "url": "/ani-ecom/static/js/runtime-main.79d5c216.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/ani-ecom/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/ani-ecom/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/ani-ecom/static/media/menu.b3f49afd.svg"
+  }
+]);
